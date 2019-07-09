d33193c56704563323be1f8aa0c24205cf765d74
annierm18 <annierm18@yahoo.com>
2019-07-08 18:50:12 -0700

    created a page title component and styled it and imported a few fonts

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class SignUp extends Component {
     render() {
+        console.log('signup')
         return (
             <div className='sign-up'>
                 sign up
