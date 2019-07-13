b2909f9c8f91a39f4ce518d1ec743a370ba99fa4
annierm18 <annierm18@yahoo.com>
2019-07-12 19:23:05 -0700

    built account information form from sign in form template

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -21,7 +21,7 @@ class SignIn extends Component {
 
         return (
             <div className='sign-in'>
-            <PageTitle className='sign-in__page-title' title='Login'/>
+                <PageTitle className='sign-in__page-title' title='Login'/>
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
