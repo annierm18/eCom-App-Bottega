35cdb875d73a70302d99eeb9682bcfb9579b8afe
annierm18 <annierm18@yahoo.com>
2019-07-14 19:32:01 -0700

    structured shop cart component and styled

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
 
@@ -47,6 +48,9 @@ class Shop extends Component {
                         })
                     }
                 </div>
+                {
+                    <ShopCart/>
+                }
             </div>
         )
     }
